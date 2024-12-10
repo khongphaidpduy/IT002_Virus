af5033d49f92d85dbee1a5b4596574e4900852fb
khongphaidpduy <23520362@gm.uit.edu.vn>
2024-12-10 18:21:48 +0700

    Update util.h

--- De19/util.h
+++ De19/util.h
@@ -1,9 +1,11 @@
 #ifndef UTIL_H
 #define UTIL_H
+
 #include <bits/stdc++.h>
+
 double RandomDouble(); // Tạo số ngẫu nhiên từ 0 đến 1
 
-extern double bangxacsuat[3][4]; 
-extern double bangxacsuat2[3][4]; 
+extern double bang1[3][4]; 
+extern double bang2[3][4]; 
 
 #endif
