eb779ed64f7bc95f6c4004c8a6279288b70212bd
khongphaidpduy <23520362@gm.uit.edu.vn>
2024-12-10 17:40:23 +0700

    Update hiv.cpp

--- De19/hiv.cpp
+++ De19/hiv.cpp
@@ -1,11 +1,11 @@
 #include "hiv.h"
 HIV::HIV(string khanangmiendich, bool Vacxin) : Virus(khanangmiendich,Vacxin) {
-	this->AvgProb = 0.9;
+	this->AvgProb = 0.05;
 	if (this->Symptom == "Trieu chung nang") {
-		this->SymptomInfo = "Sung hach keo dai, lo loet, viem phoi, tieu chay nang";
+		this->SymptomInfo = "Sot cao, ho khan, kho tho va dau dau du doi";
 	}
 	else if (this->Symptom == "Trieu chung nhe") {
-		this->SymptomInfo = "Cam, sot";
+		this->SymptomInfo = "Sot, ho, mat vi giac";
 	}
 	else {
 		this->SymptomInfo = "Khong trieu chung";
