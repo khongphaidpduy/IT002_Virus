ecab21431358396bf39469ee461bd6d32b3dc1f1
khongphaidpduy <23520362@gm.uit.edu.vn>
2024-12-10 18:11:59 +0700

    Add files via upload

---
+++ De19/util.h
@@ -0,0 +1,9 @@
+#ifndef UTIL_H
+#define UTIL_H
+#include <bits/stdc++.h>
+double RandomDouble(); // Tạo số ngẫu nhiên từ 0 đến 1
+
+extern double bangxacsuat[3][4]; 
+extern double bangxacsuat2[3][4]; 
+
+#endif
