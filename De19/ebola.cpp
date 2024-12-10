ff1d2481d6e8c199b6ea0194faeaf0af95b40010
khongphaidpduy <23520362@gm.uit.edu.vn>
2024-12-10 17:39:10 +0700

    Update ebola.cpp

--- De19/ebola.cpp
+++ De19/ebola.cpp
@@ -1,12 +1,12 @@
 #include "ebola.h"
 
 Ebola::Ebola(string ImmuneAbility, bool Vacxin) : Virus(ImmuneAbility,Vacxin) {
-	this->AvgProb = 0.5;
+	this->AvgProb = 0.05;
 	if (this->Symptom == "Trieu chung nang") {
-		this->SymptomInfo = "Non mua, tieu chay va xuat huyet ca ngoai lan ben trong";
+		this->SymptomInfo = "Sot cao, ho khan, kho tho va dau dau du doi";
 	}
 	else if (this->Symptom == "Trieu chung nhe") {
-		this->SymptomInfo = "Sot, dau hong, dau co va dau dau";
+		this->SymptomInfo = "Sot, ho, mat vi giac";
 	}
 	else {
 		this->SymptomInfo = "Khong trieu chung";
