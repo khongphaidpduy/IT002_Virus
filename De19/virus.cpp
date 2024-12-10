4877531f09dabaa06c73ac5c50ca13632c2a339d
khongphaidpduy <23520362@gm.uit.edu.vn>
2024-12-10 17:47:47 +0700

    Update virus.cpp

--- De19/virus.cpp
+++ De19/virus.cpp
@@ -1,8 +1,23 @@
 #include "virus.h"
-#include "util.h"
 #include <iostream>
 #include <string>
 using namespace std;
+
+double bangxacxuat[3][4] = {
+	{0.5, 0.35, 0.15, 0.5},
+	{0.1, 0.4, 0.5, 0.7},
+	{0.05, 0.15, 0.8, 1}
+};
+
+double bangxacxuat2[3][4] = {
+	{0.7, 0.25, 0.05, 0.4},
+	{0.2, 0.5, 0.3, 0.6},
+	{0.1, 0.4, 0.5, 0.8}
+};
+
+double RandomDouble() {
+	return (rand() % 101) / 100.0;
+}
 
 Virus::Virus(string immuneAbility, bool vacxin) 
     : ImmuneAbility(immuneAbility), Vacxin(vacxin) {
