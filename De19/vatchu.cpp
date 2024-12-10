ef8ffab87c3f9f7fc37e257eb792255be92db03e
khongphaidpduy <23520362@gm.uit.edu.vn>
2024-12-10 17:47:20 +0700

    Update vatchu.cpp

--- De19/vatchu.cpp
+++ De19/vatchu.cpp
@@ -1,5 +1,20 @@
 #include "vatchu.h"
-#include "util.h"
+
+double bangxacxuat[3][4] = {
+	{0.5, 0.35, 0.15, 0.5},
+	{0.1, 0.4, 0.5, 0.7},
+	{0.05, 0.15, 0.8, 1}
+};
+
+double bangxacxuat2[3][4] = {
+	{0.7, 0.25, 0.05, 0.4},
+	{0.2, 0.5, 0.3, 0.6},
+	{0.1, 0.4, 0.5, 0.8}
+};
+
+double RandomDouble() {
+	return (rand() % 101) / 100.0;
+}
 
 Vatchu::Vatchu(string ID, bool Vacxin) {
 	this->DeathProb = 0;
@@ -14,7 +29,6 @@ Vatchu::Vatchu(string ID, bool Vacxin) {
 		this->Symptom.push_back(this->virus[i]->TrieuChung());
 	}
 
-	
 	for (int i = 0; i < 3; i++){
 		double r2 = RandomDouble();
 		double xacsuat = this->virus[i]->TinhXacSuatTuVong();
